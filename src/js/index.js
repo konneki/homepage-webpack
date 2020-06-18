9d3ba42103c7030eeb3c5a84145659e93a2bde41
Daniel Konnek <konnekdaniel@gmail.com>
2020-06-18 10:21:09 +0200

    add opengraph protocol

--- src/js/index.js
+++ src/js/index.js
@@ -14,9 +14,7 @@ console.log(`Make yourself at home. Mi casa, es tu casa.`);
 var hamburger = document.querySelector('.hamburger');
 // On click
 hamburger.addEventListener('click', function () {
-  // Toggle class "is-active"
   hamburger.classList.toggle('is-active');
-  // Do something else, like open/close menu
 });
 
 /*
